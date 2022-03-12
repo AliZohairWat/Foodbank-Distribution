8f6ab5390d919d2857c540286dc46d828d14f538
Ryan Mitchell <rw2mitch@uwaterloo.ca>
2022-03-12 18:11:42 -0500

    Inital commit
    Let's go boys

---
+++ photos/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+class Food(models.Model):
+    class Meta:
+        verbose_name = 'Food'
+        verbose_name_plural = 'Foods'
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=30)
+    weight = models.IntegerField()
+    date = models.DateField()
+    glutenFree = models.BooleanField()
+    halal = models.BooleanField()   
+
+    def __str__(self):
+        return 'Food(category=' + str(self.category.name) + ' ,name=' + self.name + ' ,weight=' + self.weight + ' ,date=' + str(self.date) + ' ,glutenFree=' + str(self.glutenFree) + ' ,Halal=' + str(self.halal) + ')'
---
+++ photos/views.py
@@ -0,0 +1,151 @@
+from django.shortcuts import render, redirect
+from .models import Category, Photo, Food
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+import datetime
+# Create your views here.
+
+
+def loginUser(request):
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('gallery')
+
+    return render(request, 'photos/login_register.html', {'page': page})
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            if user is not None:
+                login(request, user)
+                return redirect('gallery')
+
+    context = {'form': form, 'page': page}
+    return render(request, 'photos/login_register.html', context)
+
+
+@login_required(login_url='login')
+def gallery(request):
+    user = request.user
+    category = request.GET.get('category')
+    if category == None:
+        foods = Food.objects.filter(category__user=user)
+    else:
+        foods = Food.objects.filter(
+            category__name=category, category__user=user)
+
+    categories = Category.objects.filter(user=user)
+    context = {'categories': categories, 'foods': foods}
+    return render(request, 'photos/gallery.html', context)
+
+
+@login_required(login_url='login')
+def viewPhoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
+
+@login_required(login_url='login')
+def viewFood(request, pk):
+    food = Food.objects.get(id=pk)
+    return render(request, 'foods/food.html', {'food': food})
+
+
+@login_required(login_url='login')
+def addPhoto(request):
+    user = request.user
+
+    categories = user.category_set.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        images = request.FILES.getlist('images')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                user=user,
+                name=data['category_new'])
+        else:
+            category = None
+
+        for image in images:
+            photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
+
+        return redirect('gallery')
+
+@login_required(login_url='login')
+def addFood(request):
+    user = request.user
+
+    categories = user.category_set.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        foodDict = [{'name': 'Chicken', 
+                      'weight': 5,
+                      'date': datetime.datetime(2020, 5, 17).date(),
+                      'glutenFree': True, 'halal': False},
+                     {'name': 'Beef', 
+                      'weight': 6,
+                      'date': datetime.datetime(2021, 2, 20).date(),
+                      'glutenFree': True, 'halal': False}, 
+                     {'name': 'Pork', 
+                      'weight': 8,
+                      'date': datetime.datetime(2016, 8, 18).date(),
+                      'glutenFree': True, 'halal': False}]
+
+        foodDict.push({'name': data, 
+                      'weight': 8,
+                      'date': datetime.datetime(2016, 8, 18).date(),
+                      'glutenFree': True, 'halal': False})
+
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                user=user,
+                name=data['category_new'])
+        else:
+            category = None
+        
+        for item in foodDict:
+            food = Food.objects.create(
+                category=category,
+                name=item.name,
+                weight=item.weight,
+                date=item.date,
+                glutenFree=item.glutenFree,
+                halal=item.halal,
+            )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
